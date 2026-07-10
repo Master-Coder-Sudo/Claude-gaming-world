23fb14062952aee5afd581683d77b52afbbf9098
Trev Cavill <trev.cavill@outlook.com>
2026-07-10 14:30:58 +1200

    fix(mobile): remove Appflow OTA updates

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -11,16 +11,14 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.4.1"),
-        .package(name: "CapacitorLiveUpdates", path: "../../../node_modules/@capacitor/live-updates")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.4.1")
     ],
     targets: [
         .target(
             name: "CapApp-SPM",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "CapacitorLiveUpdates", package: "CapacitorLiveUpdates")
+                .product(name: "Cordova", package: "capacitor-swift-pm")
             ]
         )
     ]
